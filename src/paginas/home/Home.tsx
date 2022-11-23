5fca089e7bca1d9a5e0a90be6cd0c4b937385220
LucasMarques97 <lucaschristian76@gmail.com>
2022-11-23 12:55:21 -0300

    Criado componente Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css'
+
+function Home (){
+    return(
+        <>
+            <h1 className='titulo'>No Way Home</h1>
+            
+            <div className='container'>
+            <img src='https://images.prismic.io/mystique/5d7c09b9-40e5-4254-ae1c-2c1cb59aa898_IMG3.jpg?auto=compress,format'/>
+            </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsam, sint fugiat iure iusto explicabo rerum at enim ipsa sapiente officia tempora a quae similique? Qui soluta impedit fuga eveniet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsam, sint fugiat iure iusto explicabo rerum at enim ipsa sapiente officia tempora a quae similique? Qui soluta impedit fuga eveniet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ipsam, sint fugiat iure iusto explicabo rerum at enim ipsa sapiente officia tempora a quae similique? Qui soluta impedit fuga eveniet.</p>
+        </>
+    )
+}
+
+export default Home
